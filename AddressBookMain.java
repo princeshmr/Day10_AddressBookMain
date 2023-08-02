119eba9a8038be3e050fdc5f8c15fa6dd5317efe
rohit789@ <princeshmr27@gmail.com>
2023-08-02 13:15:38 +0530

    Add Multiple person to Address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.bridgeLabz.Day10_AddressBookMain;
 
+import java.util.Scanner;
+
 import com.bridgeLabz.AddressBook;
 
 public class AddressBookMain {
@@ -8,16 +10,24 @@ public class AddressBookMain {
 		System.out.println("Welcome to AddressBookMain class");
 		
 		AddressBook addressBook=new AddressBook();
-		addressBook.addContacts();   //add contact only for one person contact
-		addressBook.displayContact(); //to display contact details
+		//addressBook.addContacts();   //add contact only for one person contact
+		//addressBook.displayContact(); //to display contact details
 		
 		//System.out.println("To edit the personal details");
 		//addressBook.editContact();  //if we want change contact details   
 		//addressBook.displayContact();  //To display contact after edit contact details
 		
-		System.out.println("To Delete the personal details");
-		addressBook.deleteContact();   //if we want to delete personal details
-		addressBook.displayContact();
+		//System.out.println("To Delete the personal details");
+		//addressBook.deleteContact();   //if we want to delete personal details
+		//addressBook.displayContact();
+		
+		 Scanner scan=new Scanner(System.in);
+		 System.out.println("Enter the number of contact you want to add: ");
+		 int numberOfContacts=scan.nextInt();
+	   	 for(int i=0; i<numberOfContacts; i++) {
+				addressBook.addContacts();      //add contact more than 1 person contact
+		 }
+	   	 addressBook.displayContact();
 	}
 
 }
