2dfa8516790d111281b19349c22efef05ac3520c
rohit789@ <princeshmr27@gmail.com>
2023-08-02 09:45:26 +0530

    Welcome to Address Book Program

---
+++ AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz.Day10_AddressBookMain;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBookMain class ");
+	}
+
+}
